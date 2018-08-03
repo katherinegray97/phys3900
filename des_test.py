96efb98ba55f968cb1fe3be5e16682fa7721aab0
Katie <katherine.gray@uqconnect.edu.au>
2018-08-03 17:55:11 +1000

    Alpha experimentation

--- des_test.py
+++ des_test.py
@@ -5,8 +5,9 @@ Created on Thu Jul 12 10:42:33 2018
 """
 
 import time
+
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 start = time.time()
@@ -18,11 +19,11 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
-class PlotData(object): 
+class PlotData(object):
     def __init__(self, ra, dec, z):
          """
-         The plotting data for each point in a survey 
-         
+         The plotting data for each point in a survey
+
          Params
          ra:  np.ndarray
              right ascension of each point
@@ -32,42 +33,52 @@ class PlotData(object):
              redshift of each point
          """
          self.length = len(ra)
-        
-         # Opacity 
-         self.alpha = np.ones(self.length)
-         # Size of points 
-         self.size = np.ones(self.length)
-        
+
+         # Opacity
+         self.alpha = np.zeros((self.length,4))
+         self.alpha[:,0] = 1.0
+
+         self.alpha[:,3] = np.random.uniform( 0,1,self.length)
+
+         # Size of points
+         self.size = 100
+
          # Equatorial coord system
          self.ra = ra
          self.dec = dec
          self.z = z
-        
-        
-         # Cartersion coord system 
-         self.x = np.sin(ra)* np.sin(dec) * z 
+
+
+         # Cartersion coord system
+         self.x = np.sin(ra)* np.sin(dec) * z
          self.y = np.cos(ra) * np.sin(dec) * z
          self.z = np.cos(dec) * z
-           
-    
+
+
     def translate(self, obs_x, obs_y, obs_z, obs_theta, obs_azimuth):
-        self.x = self.x + 1000
-            
-        
+        self.x = self.x + obs_x
+        self.y = self.y + obs_y
+        self.z = self.z + obs_z
+
+
 data = np.load("des_thinned.npy")
-numpoints = 100
+numpoints = 30
 des = PlotData(np.array(data[0:numpoints,0]),np.array(data[0:numpoints,1]),np.array(data[0:numpoints,2]))
 
-ax.scatter(des.x,des.y,des.z,c='b')
+ax.scatter(des.x,des.y,des.z, c=des.alpha)
+ax.scatter(0,0,0,c="r")
 
-translate(1,2,2,2,2)
+#ax.scatter(0,0.1,1,c="y")
+#
+#des.translate(0,0.1,1,2,2)
 
-ax.scatter(des.x,des.y,des.z,c='g')
+
+
 
 #print ('Executed in:' time.time()-start, 'secs')
 
 
 
 
- 
+
 
