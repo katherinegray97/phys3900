b7062dd8d4ea51c98b9b3967e0fa36c4e4c518c2
Katie <katherine.gray@uqconnect.edu.au>
2018-08-09 14:09:49 +1000

    Some formatting changes

--- des_test.py
+++ des_test.py
@@ -7,6 +7,7 @@ Created on Thu Jul 12 10:42:33 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 fig = plt.figure()
@@ -18,42 +19,41 @@ ax.set_zlabel('z')
 
 class PlotData(object):
     def __init__(self, ra, dec, z):
-         """
-         The plotting data for point in a survey. Points are stored as
-         Cartesian coordinates. Observer is initially at origin, looking down
-         x axis.
+        """
+        The plotting data for point in a survey. Points are stored as
+        Cartesian coordinates. Observer is initially at origin, looking down
+        x axis.
 
-         Params
-         ra:  np.ndarray
-             right ascension of each point
-         dec: np.ndarray
-             declination of each point
-         z:   np.ndarray
-             redshift of each point
-         """
-         # Equatorial coord system - private variables, not to be modified
-         self._ra = ra
-         self._dec = dec
-         self._z = z
+        Params
+        ra:  np.ndarray
+        right ascension of each point
+        dec: np.ndarraydeclination of each point
+        z:   np.ndarray
+        redshift of each point
+        """
 
-         # Cartersion coord system
-         dec = np.pi / 2 - dec
-         self.x = np.sin(ra)* np.sin(dec) * z
-         self.y = np.cos(ra) * np.sin(dec) * z
-         self.z = np.cos(dec) * z
+        # Equatorial coord system - private variables, not to be modified
+        self._ra = ra
+        self._dec = dec
+        self._z = z
 
-         self.obs_x = 0
-         self.obs_y = 0
-         self.obs_z = 0
-         self.obs_theta = 0
-         self.obs_phi = np.pi/2
+        # Cartersion coord system
+        dec = np.pi / 2 - dec
+        self.x = np.sin(ra)* np.sin(dec) * z
+        self.y = np.cos(ra) * np.sin(dec) * z
+        self.z = np.cos(dec) * z
 
-         # Number of data points
-         self.length = len(ra)
-         # Setting threshold - currently arbitrary
-         self.threshold = max(self.get_r())*0.8
+        self.obs_x = 0
+        self.obs_y = 0
+        self.obs_z = 0
+        self.obs_theta = 0
+        self.obs_phi = np.pi/2
+        # Number of data points
+        self.length = len(ra)
+        # Setting threshold - currently arbitrary
+        self.threshold = max(self.get_r())*0.8
 
-         self._set_perspective_data("red")
+        self._set_perspective_data("red")
 
 
     def _set_perspective_data(self, colour):
@@ -64,16 +64,16 @@ class PlotData(object):
         self.num_threshold = len(self.close_x)
 
         # Plotting size of points
-        self.size =  20*(1 - self.get_close_r()/self.threshold)
+        self.size = 20*(1 - self.get_close_r()/self.threshold)
 
         # Alpha value of points
-        self.alpha = np.zeros((self.num_threshold,4))
-        if (colour == "red"):
-            self.alpha[:,0] = 1.0
+        self.alpha = np.zeros((self.num_threshold, 4))
+        if colour == "red":
+            self.alpha[:, 0] = 1.0
         else:
-            self.alpha[:,1] = 1.0
+            self.alpha[:, 1] = 1.0
 
-        self.alpha[:,3] = 1-(self.get_close_r()*(1.0/self.threshold))
+        self.alpha[:, 3] = 1-(self.get_close_r()*(1.0/self.threshold))
 
     def translate(self, obs_x, obs_y, obs_z, obs_theta, obs_phi):
         """
@@ -110,7 +110,7 @@ class PlotData(object):
         (Note, theta is the azimuthal angle in the x-y plane, 0 < theta < 2pi)
         """
 
-        return np.where((self.y-self.obs_y) == 0, 0, np.arctan2(self.y-self.obs_y,self.x-self.obs_x))
+        return np.where((self.y-self.obs_y) == 0, 0, np.arctan2(self.y-self.obs_y, self.x-self.obs_x))
 
     def get_phi(self):
         """
@@ -127,7 +127,7 @@ class PlotData(object):
         (Note, r is the distance from the origin)
         """
 
-        return  np.sqrt(np.power(self.x-self.obs_x,2) + np.power(self.y-self.obs_y,2) + np.power(self.z-self.obs_z,2))
+        return  np.sqrt(np.power(self.x-self.obs_x, 2) + np.power(self.y-self.obs_y, 2) + np.power(self.z-self.obs_z, 2))
 
 
     def get_close_r(self):
@@ -148,17 +148,17 @@ data = data[~np.isnan(data).any(axis=1)]
 
 # Truncate, for testing
 numpoints = 1000
-des = PlotData(data[0:numpoints,0],data[0:numpoints,1],data[0:numpoints,2])
+des = PlotData(data[0:numpoints, 0], data[0:numpoints, 1], data[0:numpoints, 2])
 
 
-ax.scatter(des.x,des.y,des.z, marker = "*", c= "r", alpha = 0.05)
-ax.scatter(des.obs_x, des.obs_y, des.obs_z, c="y", marker = "o")
+ax.scatter(des.x, des.y, des.z, marker="*", c="r", alpha=0.05)
+ax.scatter(des.obs_x, des.obs_y, des.obs_z, c="y", marker="o")
 
 
-des.translate(0.5, 0.1, 0,np.pi/2, 0)
+des.translate(0.5, 0.1, 0, np.pi/2, 0)
 
-ax.scatter(des.x, des.y, des.z,marker = "*",c = 'b' ,alpha = 0.05 )
-ax.scatter(des.obs_x, des.obs_y, des.obs_z, marker = "o", c = "g", s = 40)
+ax.scatter(des.x, des.y, des.z, marker="*", c='b', alpha=0.05)
+ax.scatter(des.obs_x, des.obs_y, des.obs_z, marker="o", c="g", s=40)
 
 
 
