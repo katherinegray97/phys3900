de8f8d45c4465cfc64aa5e9e28ac7a6ec3779768
Katie <katherine.gray@uqconnect.edu.au>
2018-08-15 09:33:17 +1000

    Testing turning

--- des_vis/des_test.py
+++ des_vis/des_test.py
@@ -5,13 +5,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from plot_data import PlotData
 from translator import Translator
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
 
 # Import data
 data = np.load("des_thinned.npy")
@@ -20,19 +13,90 @@ data = np.load("des_thinned.npy")
 data = data[~np.isnan(data).any(axis=1)]
 
 # Truncate, for testing
-numpoints = 1000
+numpoints = 5000# len(data)
 des = PlotData(data[0:numpoints, 0], data[0:numpoints, 1],
                data[0:numpoints, 2])
 
-
-ax.scatter(des.x, des.y, des.z, marker="*", c="r", alpha=0.05)
-ax.scatter(des.observer.x, des.observer.y, des.observer.z, c="y", marker="o")
-
 translator = Translator(des)
 
-translator.translate(0.5, 0.1, 0, np.pi/2, 0)
+######ROTATE
+for i in range(1, 15):
+    fig = plt.figure(figsize=(8, 8), dpi=192, frameon=False)
 
-ax.scatter(translator.plot_data.x, translator.plot_data.y,
-           translator.plot_data.z, marker="*", c='b', alpha=0.05)
-ax.scatter(translator.plot_data.observer.x, translator.plot_data.observer.y,
-           translator.plot_data.observer.z, marker="o", c="g", s=40)
+    # removes subplot padding
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+
+    ax = fig.add_subplot(111, projection='3d')
+
+    # axis off, black background
+    ax.set_axis_off()
+    ax.patch.set_facecolor('black')
+
+    #set camera to be looking down x axis (y increasing on left)
+    ax.view_init(elev=0,
+                 azim=180)
+
+    ax.set_xlim(0, 3)
+    ax.set_ylim(-0.25,0.25)
+    ax.set_zlim(0, 0.15)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    ax.scatter(translator.plot_data.close_x, translator.plot_data.close_y,
+               translator.plot_data.close_z, marker="*",
+               c=translator.plot_data.alpha)
+    name_out = "outputs/turn" + str(i)
+
+    #save to file
+    fig.savefig(name_out, pad_inches=0,)
+
+    #translate
+    translator.translate(0, 0, 0,np.pi/10,0)
+    plt.close("all")
+
+### Look up
+for i in range(1, 30):
+    fig = plt.figure(figsize=(8, 8), dpi=192, frameon=False)
+
+    # removes subplot padding
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+
+    ax = fig.add_subplot(111, projection='3d')
+
+    # axis off, black background
+    ax.set_axis_off()
+    ax.patch.set_facecolor('black')
+
+    #set camera to be looking down x axis (y increasing on left)
+    ax.view_init(elev=0,
+                 azim=180)
+
+    ax.set_xlim(0,0.15)
+    ax.set_ylim(-0.15,0.15)
+    ax.set_zlim(0, 0.15)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    ax.scatter(translator.plot_data.close_x, translator.plot_data.close_y,
+               translator.plot_data.close_z, marker="*",
+               c=translator.plot_data.alpha)
+    name_out = "outputs/up" + str(i)
+
+    #save to file
+    fig.savefig(name_out,  pad_inches=0, )
+
+    #translate
+    translator.translate(0.02, -0.02, 0.02, 0, 0)
+    plt.close("all")
+
+
+
+
+
+
+
+
